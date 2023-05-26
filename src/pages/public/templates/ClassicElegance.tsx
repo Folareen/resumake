0056e301aa47f894a7ce0777122bb7fb9056f011
Folareen <sakawahab03@gmail.com>
2023-05-26 17:25:13 +0100

    done for now

--- src/pages/public/templates/ClassicElegance.tsx
+++ src/pages/public/templates/ClassicElegance.tsx
@@ -2,31 +2,235 @@ import React, { useRef } from 'react'
 import ResEl from '../../../components/ResEl'
 import ResumeContainer from '../../../components/ResumeContainer'
 
+const HeaderSection = () => (
+    <div className='header'>
+        <img src='/vite.svg' className='avatar' />
+        <div>
+            <ResEl tag="h1" className='name'>
+                JENNIFER DAVIS
+            </ResEl>
+            <ResEl tag="h3" className='profession'>
+                Project Manager
+            </ResEl>
+        </div>
+        <div className='contact'>
+            <div className='contact-item'>
+                <i className="fa-solid fa-location-dot"></i>
+                <ResEl className='address' tag='h6'>
+                    CITY OF HOUSTON, TX 77002
+                </ResEl>
+            </div>
+            <div className='contact-item'>
+                <i className="fa-solid fa-phone"></i>
+                <ResEl className='phone' tag='h6'>
+                    (555) 555-5555 |
+                </ResEl>
+            </div>
+            <div className='contact-item'>
+                <i className="fa-solid fa-envelope"></i>
+                <ResEl className='email' tag='h6'>
+                    JENNIFER.DAVIS@EMAIL.COM
+                </ResEl>
+            </div>
+            <div className='contact-item'>
+                <i class="fa-solid fa-globe"></i>
+                <ResEl className='website' tag='h6' >
+                    jenifferdavis.com
+                </ResEl>
+            </div>
+        </div>
+    </div>
+)
+const MainLeftSection = () => (
+    <div className='main-left'>
+
+        <div className='heading-item'>
+            <i className="fa-solid fa-id-card"></i>
+            <ResEl tag='h3' className='block'>
+                CAREER OBJECTIVE
+            </ResEl>
+        </div>
+
+        <ResEl className='summary'>
+            Resourceful Project Manager skilled in leading cross-functional teams and delivering projects on time and within budget. Proficient in project planning and execution, risk management, and stakeholder communication. Strong communication and problem-solving skills.
+        </ResEl>
+
+
+        <ResEl className='block empty' />
+
+        <div className='heading-item'>
+            <i className="fa-solid fa-id-card"></i>
+            <ResEl tag='h3' className='block'>
+                SKILLS
+            </ResEl>
+        </div>
+
+        <ResEl className='block'>
+            <ul >
+                <li>Product development
+                </li>
+                <li>Market research and analysis
+                </li>
+                <li>Customer needs assessment
+                </li>
+                <li> Cross-functional team leadership
+                </li>
+                <li>Product roadmap planning
+                </li>
+                <li>Agile development methodulogy
+                </li>
+                <li>Customer needs assessment
+                </li>
+            </ul>
+        </ResEl>
+
+        <ResEl className='block empty' />
+        <div className='heading-item'>
+            <i className="fa-solid fa-graduation-cap"></i>
+            <ResEl tag='h3' className='block'>
+                EDUCATION
+            </ResEl>
+        </div>
+
+        <ResEl tag='h4' className='block'>
+            Master of Business Administration -
+        </ResEl>
+        <ResEl className='block'>
+            <b>Product Management</b>
+        </ResEl>
+        <ResEl className='block'>
+            University of California, Berkeley, CA
+        </ResEl>
+
+        <ResEl tag='h6' className='empty block' />
+
+        <ResEl tag='h4' className='block'>
+            Bachelor of Science
+        </ResEl>
+        <ResEl className='block'>
+            <b>Marketing</b>
+        </ResEl>
+        <ResEl className='block'>
+            San Diego State University, San Diego, CA
+        </ResEl>
+
+    </div>
+
+
+)
+const MainRightSection = () => (
+    <div className='main-right'>
+
+        <ResEl className='block empty' />
+        <div className='heading-item'>
+            <i className="fa-solid fa-briefcase"></i>
+            <ResEl tag='h3' className='block'>
+                EXPERIENCE
+            </ResEl>
+        </div>
+
+        <ResEl tag='h4' className='block'>
+            Project Manager
+        </ResEl>
+        <ResEl tag='h5' className='block' >
+            ABC Corporation, Houston, TX. * July 2017 to Current
+        </ResEl>
+        <ResEl className='first-experience block'>
+            <ul >
+                <li>
+                    Lead project planning and execution activities, including budget development, resource allocation, risk management, and stakeholder communication.
+                </li>
+                <li>Collaborate with cross-functional teams to deliver projects on time and within budget, resulting in a 20% increase in customer satisfaction ratings.
+                </li>
+                <li> Optimize project management processes and touls, resulting in a 25% reduction in project cycle time.
+                </li>
+            </ul>
+        </ResEl>
+
+        <ResEl className='block empty' />
+        <ResEl tag='h4' className='block'>
+            Project Manager
+        </ResEl>
+        <ResEl tag='h5' className='block' >
+            ABC Corporation, Houston, TX. * July 2017 to Current
+        </ResEl>
+        <ResEl className='first-experience block'>
+            <ul >
+                <li>
+                    Lead project planning and execution activities, including budget development, resource allocation, risk management, and stakeholder communication.
+                </li>
+                <li>Collaborate with cross-functional teams to deliver projects on time and within budget, resulting in a 20% increase in customer satisfaction ratings.
+                </li>
+                <li> Optimize project management processes and touls, resulting in a 25% reduction in project cycle time.
+                </li>
+            </ul>
+        </ResEl>
+
+        <ResEl className='block empty' />
+        <ResEl tag='h4' className='block'>
+            Project Manager
+        </ResEl>
+        <ResEl tag='h5' className='block' >
+            ABC Corporation, Houston, TX. * July 2017 to Current
+        </ResEl>
+        <ResEl className='first-experience block'>
+            <ul >
+                <li>
+                    Lead project planning and execution activities, including budget development, resource allocation, risk management, and stakeholder communication.
+                </li>
+                <li>Collaborate with cross-functional teams to deliver projects on time and within budget, resulting in a 20% increase in customer satisfaction ratings.
+                </li>
+                <li> Optimize project management processes and touls, resulting in a 25% reduction in project cycle time.
+                </li>
+            </ul>
+        </ResEl>
+
+
+        <ResEl className='block empty' />
+        <ResEl tag='h3' className='block'>
+            PROJECTS
+        </ResEl>
+
+        <ResEl tag='h5' className='block'>
+            Project Manager // Houston, TX // July 2017 to Current
+        </ResEl>
+        <ResEl tag='h4' className='block' >
+            ABC Corporation
+        </ResEl>
+        <ResEl className='first-experience block'>
+            <ul >
+                <li>
+                    Lead project planning and execution activities, including budget development, resource allocation, risk management, and stakeholder communication.
+                </li>
+                <li>Collaborate with cross-functional teams to deliver projects on time and within budget, resulting in a 20% increase in customer satisfaction ratings.
+                </li>
+                <li> Optimize project management processes and touls, resulting in a 25% reduction in project cycle time.
+                </li>
+            </ul>
+        </ResEl>
+
+    </div>
+)
+const FooterSection = () => (
+    <div>
+    </div>
+)
+
+
 const ClassicElegance = () => {
-    const resumeRef = useRef<HTMLDivElement | null>(null)
+    const resumeRef = useRef<React.MutableRefObject<HTMLDivElement> | null>(null)
+
 
     return (
-        <ResumeContainer resumeRef={resumeRef}>
-            <div className='resume' ref={resumeRef}>
-
-                <ResEl className='header'>
-                    Dave mark
-                </ResEl>
-                <ResEl className='address'>
-                    Address
-                </ResEl>
-                <ResEl className='position' >
-                    developer
-                </ResEl>
-                <ResEl>
-                    hello
-                </ResEl>
-                <ResEl>
-                    you
-                </ResEl>
-
-            </div>
-        </ResumeContainer>
+        <ResumeContainer
+            resumeRef={resumeRef} resClassName='classic-elegance'
+            isSectioned={true}
+            HeaderSection={HeaderSection}
+            MainLeftSection={MainLeftSection}
+            MainRightSection={MainRightSection}
+            FooterSection={FooterSection}
+            mainLayout={'col-30-70'}
+        />
     )
 }
 
