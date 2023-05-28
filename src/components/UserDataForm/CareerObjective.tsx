992f5e11b7ef9e6083be983ef4d2e360956a3c26
Folareen <sakawahab03@gmail.com>
2023-05-28 08:43:47 +0100

    done with user career objective form

--- src/components/UserDataForm/CareerObjective.tsx
+++ src/components/UserDataForm/CareerObjective.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
 const CareerObjective = () => {
+    const [careerObjective, setCareerObjective] = useState<string>('')
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'careerObjective',
+            value: careerObjective
+        }))
+    }
+
     return (
-        <div>CareerObjective
-            <input type={'text'} />
+        <div>
+            Career Objective
+            <form>
+                <input type='text' value={careerObjective} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setCareerObjective(e.target.value)
+                }} placeholder='Career Objective' />
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form>
         </div>
     )
 }
