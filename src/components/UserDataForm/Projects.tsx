cfe0ce9a74f5677368cfaddc2db74d7c9d42ab85
Folareen <sakawahab03@gmail.com>
2023-05-28 10:11:09 +0100

    done with user projects form

--- src/components/UserDataForm/Projects.tsx
+++ src/components/UserDataForm/Projects.tsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
-const Projects = () => {
-    return (
-        <div>Projects
-            <input type='text' />
-        </div>
-    )
+type ProjectType = {
+    title: string,
+    description: string[],
+    links: string[]
 }
 
+const Projects = () => {
+    const [projects, setProjects] = useState<ProjectType[]>([])
+    const [project, setProject] = useState<{
+        title: string,
+        description: string,
+        links: string
+    }>({
+        title: '',
+        description: '',
+        links: ''
+    })
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'projects',
+            value: projects
+        }))
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setProject({
+            ...project,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div>
+            Projects
+            <form>
+                <p>
+                    {projects.map((project, index) => {
+                        return (
+                            <span key={index}>{JSON.stringify(project)} , </span>
+                        )
+                    })
+                    }
+                </p>
+                <div>
+                    <div>
+                        <input type='text' value={project.title} onChange={onChange} placeholder='Project Title' name='title' />
+                        <textarea value={project.description} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                            setProject({
+                                ...project,
+                                [e.target.name]: e.target.value
+                            })
+                        }} placeholder='project description' name='description' />
+                        <textarea value={project.links} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                            setProject({
+                                ...project,
+                                [e.target.name]: e.target.value
+                            })
+                        }} placeholder='project links' name='links' />
+                    </div>
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                        e.preventDefault()
+                        if (project.title && project.description && project.links) {
+                            setProjects([...projects, {
+                                title: project.title,
+                                description: project.description.split('\n'),
+                                links: project.links.split('\n')
+                            }])
+                            setProject({
+                                title: '',
+                                description: '',
+                                links: ''
+                            })
+                        }
+                    }}>
+                        Add
+                    </button>
+                </div>
+
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form >
+        </div >
+    )
+
+}
 export default Projects
