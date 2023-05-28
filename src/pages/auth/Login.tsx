ec649344986308db7841689586f9e492a1a4a75c
Folareen <sakawahab03@gmail.com>
2023-05-28 21:59:24 +0100

    done with login setup

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
+import { auth } from '../../../firebase.config'
+import formatFirebaseError from '../../utils/formatFirebaseError'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [submitting, setSubmitting] = useState(false)
+
+    const handleLogin = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        try {
+            e.preventDefault()
+            setSubmitting(true)
+            await signInWithEmailAndPassword(auth, email, password)
+        } catch (error: any) {
+            console.log(error)
+            toast.error(formatFirebaseError(error.message))
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
     return (
-        <div>Login</div>
+        <div>Login
+            <form>
+                <input type='email' value={email} onChange={(e) => {
+                    setEmail(e.target.value)
+                }} placeholder='email' />
+                <input type='password' placeholder='password' value={password} onChange={(e) => {
+                    setPassword(e.target.value)
+                }} />
+                <button onClick={handleLogin}>
+                    {
+                        submitting ?
+                            'submitting'
+                            :
+                            'Login'
+                    }
+                </button>
+            </form>
+        </div>
     )
 }
 
