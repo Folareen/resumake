8dd7acb1c81469ab651810bcd57e1102d5ac2e16
Folareen <sakawahab03@gmail.com>
2023-05-28 10:57:26 +0100

    done with user languages form

--- src/components/UserDataForm/Languages.tsx
+++ src/components/UserDataForm/Languages.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
 const Languages = () => {
+    const [languages, setLanguages] = useState<string[]>([])
+    const languageRef = React.useRef<HTMLInputElement>(null)
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'languages',
+            value: languages
+        }))
+    }
+
     return (
-        <div>Languages
-            <input type='text' />
+        <div>
+            Languages
+            <form>
+                <p>
+                    {languages.map((language, index) => {
+                        return (
+                            <span key={index}>{language} , </span>
+                        )
+                    })
+                    }
+                </p>
+                <div>
+                    <input type='text' ref={languageRef} placeholder='language' />
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                        e.preventDefault()
+                        if (languageRef.current) {
+                            setLanguages([...languages, languageRef.current.value])
+                            languageRef.current.value = ''
+                        }
+                    }}>
+                        Add
+                    </button>
+                </div>
+
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form>
         </div>
     )
 }
