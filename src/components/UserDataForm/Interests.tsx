94683f4e3d82eec8e195387b7991bbf57e80628d
Folareen <sakawahab03@gmail.com>
2023-05-28 10:56:38 +0100

    done with user interests form

--- src/components/UserDataForm/Interests.tsx
+++ src/components/UserDataForm/Interests.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
 const Interests = () => {
+    const [interests, setInterests] = useState<string[]>([])
+    const interestRef = React.useRef<HTMLInputElement>(null)
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'interests',
+            value: interests
+        }))
+    }
+
     return (
-        <div>Interests</div>
+        <div>
+            Interests
+            <form>
+                <p>
+                    {interests.map((interest, index) => {
+                        return (
+                            <span key={index}>{interest} , </span>
+                        )
+                    })
+                    }
+                </p>
+                <div>
+                    <input type='text' ref={interestRef} placeholder='interest' />
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                        e.preventDefault()
+                        if (interestRef.current) {
+                            setInterests([...interests, interestRef.current.value])
+                            interestRef.current.value = ''
+                        }
+                    }}>
+                        Add
+                    </button>
+                </div>
+
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form>
+        </div>
     )
 }
 
