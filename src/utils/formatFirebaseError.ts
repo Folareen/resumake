f6e4bbd8eedc7cbb23a771f1a26b81473f9783da
Folareen <sakawahab03@gmail.com>
2023-05-28 22:01:03 +0100

    format firebase error

---
+++ src/utils/formatFirebaseError.ts
@@ -0,0 +1,13 @@
+const formatFirebaseError = (rawErrorMessage : string) => {
+  let errorMessageArr = rawErrorMessage
+    .slice(22, rawErrorMessage.length - 2)
+    .split("-");
+
+  errorMessageArr = errorMessageArr.map((message) => {
+    return message[0].toUpperCase() + message.slice(1, message.length);
+  });
+
+  let errorMessage = errorMessageArr.join(" ");
+  return errorMessage;
+};
+export default formatFirebaseError;
