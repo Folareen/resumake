dd76cebfcfe0d198025270f1056a05267d44b5f1
Folareen <sakawahab03@gmail.com>
2023-05-28 08:51:34 +0100

    done with user skills form

--- src/components/UserDataForm/Skills.tsx
+++ src/components/UserDataForm/Skills.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
 const Skills = () => {
+    const [skills, setSkills] = useState<string[]>([])
+    const skillRef = React.useRef<HTMLInputElement>(null)
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'skills',
+            value: skills
+        }))
+    }
+
     return (
-        <div>Skills
-            <input type='text' />
+        <div>Profile
+            <form>
+                <p>
+                    {skills.map((skill, index) => {
+                        return (
+                            <span key={index}>{skill} , </span>
+                        )
+                    })
+                    }
+                </p>
+                <div>
+                    <input type='text' ref={skillRef} placeholder='skill' />
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                        e.preventDefault()
+                        if (skillRef.current) {
+                            setSkills([...skills, skillRef.current.value])
+                            skillRef.current.value = ''
+                        }
+                    }}>
+                        Add
+                    </button>
+                </div>
+
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form>
         </div>
     )
 }
