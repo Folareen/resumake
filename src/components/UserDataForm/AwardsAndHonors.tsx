ddb83424749b88e7182697be2f4b68fe722ff1ce
Folareen <sakawahab03@gmail.com>
2023-05-28 10:42:25 +0100

    done with user awards and honors form

--- src/components/UserDataForm/AwardsAndHonors.tsx
+++ src/components/UserDataForm/AwardsAndHonors.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
-const AwardsAndHonors = () => {
-    return (
-        <div>AwardsAndHonors
-            <input type='text' />
-        </div>
-    )
+type AwardsAndHonorsType = {
+    title: string,
+    date: string,
 }
 
-export default AwardsAndHonors
+const AwardsAndHonours = () => {
+    const [awardsAndHonors, setAwardsAndHonors] = useState<AwardsAndHonorsType[]>([])
+    const [awardsAndHonorsItem, setAwardsAndHonorsItem] = useState<AwardsAndHonorsType>({
+        title: '',
+        date: '',
+    })
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'awardsAndHonors',
+            value: awardsAndHonors
+        }))
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAwardsAndHonorsItem({
+            ...awardsAndHonorsItem,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div>
+            Awards and Honors
+            <form>
+                <p>
+                    {awardsAndHonors.map((awardAndHonoursItem, index) => {
+                        return (
+                            <span key={index}>{JSON.stringify(awardAndHonoursItem)} , </span>
+                        )
+                    })
+                    }
+                </p>
+                <div>
+                    <div>
+                        <input type='text' value={awardsAndHonorsItem.title} onChange={onChange} placeholder='title' name='title' />
+                        <input type='date' value={awardsAndHonorsItem.date} onChange={onChange} placeholder='date' name='date' />
+                    </div>
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                        e.preventDefault()
+                        if (awardsAndHonorsItem.title && awardsAndHonorsItem.date) {
+                            setAwardsAndHonors([...awardsAndHonors, awardsAndHonorsItem])
+                            setAwardsAndHonorsItem({
+                                title: '',
+                                date: '',
+                            })
+                        }
+                    }}>
+                        Add
+                    </button>
+                </div>
+
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form >
+        </div >
+    )
+
+}
+export default AwardsAndHonours
