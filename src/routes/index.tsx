1f4f341fb3c7b9d48ebd00ea7c38ddf9927f0e69
Folareen <sakawahab03@gmail.com>
2023-05-28 21:44:45 +0100

    done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,38 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import PublicRoutes from "./PublicRoutes"
 import PrivateRoutes from "./PrivateRoutes"
 import { RootState } from "../redux/store"
+import { useEffect } from 'react'
+import { auth } from "../../firebase.config"
+import { onAuthStateChanged } from "firebase/auth"
+import { setUser } from "../redux/features/authSlice"
 
 export default () => {
-    const { user } = useSelector((state: RootState) => state.auth)
+    const { user, loading } = useSelector((state: RootState) => state.auth)
+
+    const dispatch = useDispatch()
+
+    useEffect(
+        () => {
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    dispatch(setUser(user))
+                } else {
+                    dispatch(setUser(null))
+                }
+            })
+        }, [user]
+    )
+
+    if (loading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
 
     return (
         <BrowserRouter>
