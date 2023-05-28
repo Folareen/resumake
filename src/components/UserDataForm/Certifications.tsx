a9478707692ab578e8081c084e50f78bd93c403c
Folareen <sakawahab03@gmail.com>
2023-05-28 10:26:23 +0100

    done with user certifications form

--- src/components/UserDataForm/Certifications.tsx
+++ src/components/UserDataForm/Certifications.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../redux/features/resumeSlice'
 
-const Certifications = () => {
-    return (
-        <div>Certifications
-            <input type='text' />
-        </div>
-    )
+type CertificationType = {
+    title: string,
+    school: string,
+    date: string,
 }
 
-export default Certifications
+const Certification = () => {
+    const [certifications, setCertifications] = useState<CertificationType[]>([])
+    const [certification, setCertification] = useState<CertificationType>({
+        title: '',
+        school: '',
+        date: '',
+    })
+
+    const dispatch = useDispatch()
+
+    const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        dispatch(setUserData({
+            key: 'certifications',
+            value: certifications
+        }))
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCertification({
+            ...certification,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div>
+            Certifications
+            <form>
+                <p>
+                    {certifications.map((certification, index) => {
+                        return (
+                            <span key={index}>{JSON.stringify(certification)} , </span>
+                        )
+                    })
+                    }
+                </p>
+                <div>
+                    <div>
+                        <input type='text' value={certification.title} onChange={onChange} placeholder='title' name='title' />
+                        <input type='text' value={certification.school} onChange={onChange} placeholder='school' name='school' />
+                        <input type='date' value={certification.date} onChange={onChange} placeholder='date' name='date' />
+                    </div>
+                    <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                        e.preventDefault()
+                        if (certification.title && certification.school && certification.date) {
+                            setCertifications([...certifications, certification])
+                            setCertification({
+                                title: '',
+                                school: '',
+                                date: '',
+                            })
+                        }
+                    }}>
+                        Add
+                    </button>
+                </div>
+
+                <button onClick={onSave}>
+                    Save
+                </button>
+            </form >
+        </div >
+    )
+
+}
+export default Certification
