61b822246deb626272b84b60d7523cc7ab75202a
Folareen <sakawahab03@gmail.com>
2023-05-30 19:11:42 +0100

    done with forgot password

--- src/pages/auth/ForgotPassword.tsx
+++ src/pages/auth/ForgotPassword.tsx
@@ -1,64 +1,71 @@
 import { sendPasswordResetEmail } from 'firebase/auth'
 import React, { useState } from 'react'
+import { MdEmail } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { auth } from '../../../firebase.config'
+import GoogleSignInBtn from '../../components/GoogleSignInBtn'
 import formatFirebaseError from '../../utils/formatFirebaseError'
 
 const ForgotPassword = () => {
-    const [email, setEmail] = useState<string>('')
-    const [submitting, setSubmitting] = useState<boolean>(false)
-
+    const [email, setEmail] = useState('')
+    const [submitting, setSubmitting] = useState(false)
 
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault()
         try {
+            e.preventDefault()
             setSubmitting(true)
             await sendPasswordResetEmail(auth, email)
-            toast.success('Password reset link sent to your email!')
+            toast.success('Password reset email sent')
         } catch (error: any) {
+            console.log(error)
             toast.error(formatFirebaseError(error.message))
         } finally {
             setSubmitting(false)
         }
     }
+
+    const onFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+        e.target.parentElement.classList.add('focus')
+    }
+    const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        e.target.parentElement.classList.remove('focus')
+    }
+
     return (
-        <div>ForgotPassword
+        <div className='auth-page'>
+            <form className='auth-page__form'>
 
-            <form className=''>
-                <div>
-                    <label className='text-[#063A4F] my-2 text-md md:text-lg font-bold'>
-                        Email</label>
-                    <input type='email' className='' placeholder='example@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <h1>
+                    Forgot password
+                </h1>
+
+                <div className='input-box focus'>
+                    <MdEmail />
+                    <input type='email' value={email} onChange={(e) => {
+                        setEmail(e.target.value)
+                    }} placeholder='Email Address' className='input' onFocus={onFocus} onBlur={onBlur} />
                 </div>
 
-                <div className=''>
-                    <p className=''>
-                        Remember password?
-                    </p>
-                    <p>
-                        <Link to='/login' className=''>Login</Link>
-                    </p>
-                </div>
-                <div className=''>
-                    <p>
-                        Don't have an account?
-                    </p>
-                    <p>
-                        <Link to='/signup' className=''>Signup</Link>
-                    </p>
-                </div>
-
-                <button className={''} onClick={handleSubmit} type="submit">
+                <button onClick={handleSubmit} className='submit-btn'>
                     {
                         submitting ?
-                            <>
-                                Please wait...
-                            </>
+                            'submitting'
                             :
-                            'Reset Password'
+                            'Reset password'
                     }
                 </button>
+                <div className='have'>
+                    <p>
+                        Remember password?
+                    </p>
+                    <Link to='/login' className=''>Login</Link>
+                </div>
+
+                <div className='divider'>
+                </div>
+
+                <GoogleSignInBtn />
 
             </form>
         </div>
