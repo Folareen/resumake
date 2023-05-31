c214b9c60ad38f74084867235f901226b63a56da
Folareen <sakawahab03@gmail.com>
2023-05-31 14:42:29 +0100

    done with awards and honors form

--- src/components/UserDataForm/AwardsAndHonors.tsx
+++ src/components/UserDataForm/AwardsAndHonors.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { IoIosAdd } from 'react-icons/io'
 import { useDispatch } from 'react-redux'
 import { setUserData } from '../../redux/features/resumeSlice'
 
@@ -8,62 +9,84 @@ type AwardsAndHonorsType = {
 }
 
 const AwardsAndHonours = () => {
-    const [awardsAndHonors, setAwardsAndHonors] = useState<AwardsAndHonorsType[]>([])
-    const [awardsAndHonorsItem, setAwardsAndHonorsItem] = useState<AwardsAndHonorsType>({
-        title: '',
-        date: '',
-    })
+    const [awardsAndHonors, setAwardsAndHonors] = useState<AwardsAndHonorsType[]>([
+        {
+            title: ' ',
+            date: ' '
+        }
+    ])
+    const inputContainerRef = useRef()
 
     const dispatch = useDispatch()
 
     const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault()
         if (awardsAndHonors.length === 0) return
+        const filteredAwardsAndHonors = awardsAndHonors.filter(
+            (awardAndHonour) => {
+                return awardAndHonour.title !== '' && awardAndHonour.date.length !== 0
+            }
+        )
         dispatch(setUserData({
             key: 'awardsAndHonors',
-            value: awardsAndHonors
+            value: filteredAwardsAndHonors
         }))
     }
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAwardsAndHonorsItem({
-            ...awardsAndHonorsItem,
-            [e.target.name]: e.target.value
-        })
+    const onChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>, index: number) => {
+        let allAwardsAndHonors = awardsAndHonors
+        let currAwardsAndHonor = awardsAndHonors[index]
+        currAwardsAndHonor[e.target.name] = e.target.value
+        allAwardsAndHonors[index] = currAwardsAndHonor
+        setAwardsAndHonors(allAwardsAndHonors)
     }
+
+    useEffect(() => {
+        const emptyAwardsAndHonorsExists = awardsAndHonors.find(awardAndHonour => {
+            return awardAndHonour.title === '' || awardAndHonour.date === ''
+        })
+        if (emptyAwardsAndHonorsExists) {
+            setAwardsAndHonors(awardsAndHonors.filter(awardAndHonour => {
+                return awardAndHonour.title !== '' && awardAndHonour.date !== ''
+            }))
+        }
+    }, [awardsAndHonors])
+
+    useEffect(() => {
+        if (awardsAndHonors.length === 1) return
+        inputContainerRef.current.scrollLeft += 300
+    }, [awardsAndHonors])
+
 
     return (
         <div>
-            Awards and Honors
             <form>
-                <p>
-                    {awardsAndHonors.map((awardAndHonoursItem, index) => {
-                        return (
-                            <span key={index}>{JSON.stringify(awardAndHonoursItem)} , </span>
-                        )
-                    })
+                <h2>
+                    Awards and Honors
+                </h2>
+                <div className='multiple-input-container multiple' ref={inputContainerRef}>
+                    {
+                        awardsAndHonors.map((awardAndHonour, index) => {
+                            return (
+                                <div key={index} className='input-item'>
+                                    <input type='text' onChange={(e) => onChange(e, index)} placeholder='title' name='title' />
+                                    <input type='date' onChange={(e) => onChange(e, index)} placeholder='date' name='date' />
+                                </div>
+                            )
+                        })
                     }
-                </p>
-                <div>
-                    <div>
-                        <input type='text' value={awardsAndHonorsItem.title} onChange={onChange} placeholder='title' name='title' />
-                        <input type='date' value={awardsAndHonorsItem.date} onChange={onChange} placeholder='date' name='date' />
-                    </div>
                     <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
                         e.preventDefault()
-                        if (awardsAndHonorsItem.title && awardsAndHonorsItem.date) {
-                            setAwardsAndHonors([...awardsAndHonors, awardsAndHonorsItem])
-                            setAwardsAndHonorsItem({
-                                title: '',
-                                date: '',
-                            })
-                        }
-                    }}>
-                        Add
+                        setAwardsAndHonors([...awardsAndHonors, {
+                            title: ' ',
+                            date: ' ',
+                        }])
+                    }} className='add'>
+                        <IoIosAdd />
                     </button>
                 </div>
 
-                <button onClick={onSave} disabled={awardsAndHonors.length === 0}>
+                <button onClick={onSave} disabled={awardsAndHonors.length === 0} className='save'>
                     Save
                 </button>
             </form >
