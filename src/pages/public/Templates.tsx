70bad7de3c2b3a0d907941a85b0895a0734c6701
Folareen <sakawahab03@gmail.com>
2023-05-31 06:21:38 +0100

    temporarily commented other templates..

--- src/pages/public/Templates.tsx
+++ src/pages/public/Templates.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import BoldAmbition from './templates/BoldAmbition'
 import ClassicElegance from './templates/ClassicElegance'
@@ -12,64 +12,85 @@ import SharpFocus from './templates/SharpFocus'
 import SleekLines from './templates/SleekLines'
 
 const Templates = () => {
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }, [])
+
+
+
     return (
         <div className='templates'>
             <h2 style={{ textAlign: 'center' }}>
                 Choose a template
             </h2>
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }} className='templates__container'>
-                {
-                    [
+
+            {
+                loading ?
+                    <div className='templates__loading'>
+                        <h1>Loading...</h1>
+                    </div>
+                    :
+                    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }} className='templates__container'>
                         {
-                            title: 'Classic Elegance',
-                            template: <ClassicElegance />
-                        },
-                        {
-                            title: 'Modern Minimalism',
-                            template: <ModernMinimalism />
-                        },
-                        {
-                            title: 'Professional Edge',
-                            template: <ProfessionalEdge />
-                        },
-                        {
-                            title: 'Creative Spark',
-                            template: <CreativeSpark />
-                        },
-                        {
-                            title: 'Sleek Lines',
-                            template: <SleekLines />
-                        },
-                        // {
-                        //     title: 'Distinctive Style',
-                        //     template: <DistinctiveStyle />
-                        // },
-                        {
-                            title: 'Clean and Simple',
-                            template: <CleanAndSimple />
-                        },
-                        // {
-                        //     title: 'Refined Grace',
-                        //     template: <RefinedGrace />
-                        // },
-                        {
-                            title: 'Sharp Focus',
-                            template: <SharpFocus />
-                        },
-                        // {
-                        //     title: 'Bold Ambition',
-                        //     template: <BoldAmbition />
-                        // }
-                    ].map(({ title, template }, index) => (
-                        <Link className='template-card' to={`/create/${title.toLowerCase().split(' ').join('-')}`} >
-                            {template}
-                            <span className='title'>
-                                {title}
-                            </span>
-                        </Link>
-                    ))
-                }
-            </div>
+                            [
+                                {
+                                    title: 'Classic Elegance',
+                                    template: <ClassicElegance />
+                                },
+                                // {
+                                //     title: 'Modern Minimalism',
+                                //     template: <ModernMinimalism />
+                                // },
+                                // {
+                                //     title: 'Professional Edge',
+                                //     template: <ProfessionalEdge />
+                                // },
+                                // {
+                                //     title: 'Creative Spark',
+                                //     template: <CreativeSpark />
+                                // },
+                                // {
+                                //     title: 'Sleek Lines',
+                                //     template: <SleekLines />
+                                // },
+                                // {
+                                //     title: 'Distinctive Style',
+                                //     template: <DistinctiveStyle />
+                                // },
+                                // {
+                                //     title: 'Clean and Simple',
+                                //     template: <CleanAndSimple />
+                                // },
+                                // {
+                                //     title: 'Refined Grace',
+                                //     template: <RefinedGrace />
+                                // },
+                                // {
+                                //     title: 'Sharp Focus',
+                                //     template: <SharpFocus />
+                                // },
+                                // {
+                                //     title: 'Bold Ambition',
+                                //     template: <BoldAmbition />
+                                // }
+                            ].map(({ title, template }, index) => (
+                                <Link className='template-card' to={`/create/${title.toLowerCase().split(' ').join('-')}`} >
+                                    {template}
+                                    <span className='title'>
+                                        {title}
+                                    </span>
+                                </Link>
+                            ))
+                        }
+                    </div>
+            }
+
+
         </div>
     )
 }
