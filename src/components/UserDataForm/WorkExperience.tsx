414f0e472abd49e92dbfc9f001cb348c5179c7c7
Folareen <sakawahab03@gmail.com>
2023-05-31 12:58:52 +0100

    done with work experience form

--- src/components/UserDataForm/WorkExperience.tsx
+++ src/components/UserDataForm/WorkExperience.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { IoIosAdd } from 'react-icons/io'
 import { useDispatch } from 'react-redux'
 import { setUserData } from '../../redux/features/resumeSlice'
 
@@ -7,88 +8,100 @@ type WorkExperienceType = {
     company: string,
     startDate: string,
     endDate: string,
-    jobDescription: string[]
+    jobDescription: string
 }
 
 const WorkExperience = () => {
-    const [workExperiences, setWorkExperiences] = useState<WorkExperienceType[]>([])
-    const [workExperience, setWorkExperience] = useState<{
-        jobTitle: string,
-        company: string,
-        startDate: string,
-        endDate: string,
-        jobDescription: string
-    }>({
-        jobTitle: '',
-        company: '',
-        startDate: '',
-        endDate: '',
-        jobDescription: ''
-    })
+    const [workExperiences, setWorkExperiences] = useState<WorkExperienceType[]>([{
+        jobTitle: ' ',
+        company: ' ',
+        startDate: ' ',
+        endDate: ' ',
+        jobDescription: ' '
+    }])
+    const inputContainerRef = useRef()
 
     const dispatch = useDispatch()
 
     const onSave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault()
         if (workExperiences.length === 0) return
+        const filteredWorkExperiences = workExperiences.filter(workExperience => {
+            return workExperience.jobTitle !== '' && workExperience.company !== '' && workExperience.startDate !== '' && workExperience.endDate !== '' && workExperience.jobDescription.length > 0
+        })
         dispatch(setUserData({
             key: 'workExperience',
-            value: workExperiences
+            value: workExperiences.map(workExperience => {
+                return {
+                    ...workExperience,
+                    jobDescription: workExperience.jobDescription.split('\n')
+                }
+            })
         }))
     }
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setWorkExperience({
-            ...workExperience,
-            [e.target.name]: e.target.value
-        })
+    const onChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>, index: number) => {
+        let allWorkExperiences = workExperiences
+        let currWorkExperience = workExperiences[index]
+        currWorkExperience[e.target.name] = e.target.value
+        allWorkExperiences[index] = currWorkExperience
+        setWorkExperiences(allWorkExperiences)
     }
+
+    useEffect(() => {
+        const emptyWorkExperienceExists = workExperiences.find(workExperience => {
+            return workExperience.jobTitle === '' || workExperience.company === '' || workExperience.startDate === '' || workExperience.endDate === '' || workExperience.jobDescription === ''
+        })
+        if (emptyWorkExperienceExists) {
+            setWorkExperiences(workExperiences.filter(workExperience => {
+                return workExperience.jobTitle !== '' && workExperience.company !== '' && workExperience.startDate !== '' && workExperience.endDate !== '' && workExperience.jobDescription !== ''
+            }))
+        }
+    }, [workExperiences])
+
+    useEffect(() => {
+        inputContainerRef.current.scrollLeft += 300
+    }, [workExperiences])
+
 
     return (
         <div>
-            Work Experience
             <form>
-                <p>
+                <h2>
+                    Work Experience
+                </h2>
+
+                <div className='multiple-input-container multiple' ref={inputContainerRef}>
                     {workExperiences.map((workExperience, index) => {
                         return (
-                            <span key={index}>{JSON.stringify(workExperience)} , </span>
+                            <>
+                                <div className='input-item'>
+                                    <input type='text' onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e, index)} placeholder='Job title' name='jobTitle' className='' />
+
+                                    <input type='text' onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e, index)} placeholder='company' name='company' />
+
+                                    <input type='date' onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e, index)} placeholder='Start date' name='startDate' />
+
+                                    <input type='date' onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e, index)} placeholder='End date' name='endDate' />
+
+                                    <textarea onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => onChange(e, index)} placeholder='Job description' name='jobDescription' />
+
+                                </div>
+                            </>
                         )
                     })
                     }
-                </p>
-                <div>
-                    <div>
-                        <input type='text' value={workExperience.jobTitle} onChange={onChange} placeholder='Job Title' name='jobTitle' />
-                        <input type='text' value={workExperience.company} onChange={onChange} placeholder='company' name='company' />
-                        <input type='date' value={workExperience.startDate} onChange={onChange} placeholder='start date' name='startDate' />
-                        <input type='date' value={workExperience.endDate} onChange={onChange} placeholder='end date' name='endDate' />
-                        <textarea value={workExperience.jobDescription} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                            setWorkExperience({
-                                ...workExperience,
-                                [e.target.name]: e.target.value
-                            })
-                        }} placeholder='job description' name='jobDescription' />
-                    </div>
                     <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
                         e.preventDefault()
-                        if (workExperience.jobTitle && workExperience.company && workExperience.startDate && workExperience.endDate && workExperience.jobDescription.length > 0) {
-                            setWorkExperiences([...workExperiences, {
-                                jobTitle: workExperience.jobTitle,
-                                company: workExperience.company,
-                                startDate: workExperience.startDate,
-                                endDate: workExperience.endDate,
-                                jobDescription: workExperience.jobDescription.split('\n')
-                            }])
-                            setWorkExperience({
-                                jobTitle: '',
-                                company: '',
-                                startDate: '',
-                                endDate: '',
-                                jobDescription: ''
-                            })
-                        }
-                    }}>
-                        Add
+                        setWorkExperiences([...workExperiences, {
+                            jobTitle: ' ',
+                            company: ' ',
+                            startDate: ' ',
+                            endDate: ' ',
+                            jobDescription: ' '
+                        }])
+                    }} className='add'>
+                        <IoIosAdd />
                     </button>
                 </div>
 
