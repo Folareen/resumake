a6a84e950f2c4b128ebf0dd178ba0ec982b4eb6c
Folareen <sakawahab03@gmail.com>
2023-06-01 23:46:09 +0100

    done with 404 page

--- src/pages/public/NotFound.tsx
+++ src/pages/public/NotFound.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='not-found'>
+
+      <div className="not-found__content">
+        <h2>
+          4<span>0</span>4
+        </h2>
+        <p>
+          Page not found
+        </p>
+        <Link to='/' className=''>
+          Go  home
+        </Link>
+      </div>
+
+    </div>
   )
 }
 
